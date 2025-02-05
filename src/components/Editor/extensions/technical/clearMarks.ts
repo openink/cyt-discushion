4ce5808d4b0956edeb494cffd42f4d7c5479972f
LJM12914 <LJM12914@outlook.com>
2025-02-05 22:50:10 +0800

    修复

--- src/components/Editor/extensions/technical/clearMarks.ts
+++ src/components/Editor/extensions/technical/clearMarks.ts
@@ -16,13 +16,14 @@ const ClearMarks = Extension.create({
             if(!dispatch) return true;
             const
             { selection } = state,
-            { ranges } = selection,
+            { ranges, empty } = selection,
             { marks } = state.schema;
             for(let markName in marks) if(marks[markName] instanceof MarkType) for(let i = 0; i < ranges.length; i++){
-                //如果是区间选择，则会清除格式
-                tr.removeMark(ranges[i].$from.pos, ranges[i].$to.pos, marks[markName]);
                 //如果是空选择（光标），则会清除后续格式
-                tr.removeStoredMark(marks[markName]);
+                if(empty) tr.removeStoredMark(marks[markName]);
+                //如果是区间选择，则会清除格式
+                else tr.removeMark(ranges[i].$from.pos, ranges[i].$to.pos, marks[markName]);
+                console.log("cleared", markName);
             }
             return true;
         }
